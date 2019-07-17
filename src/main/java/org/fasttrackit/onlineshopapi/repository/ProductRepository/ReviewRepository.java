c9b92da174f1b48c67e126ece6b292a54bd9e1c9
codBogdan <1qaz2wsx*9632>
2019-07-17 22:08:28 +0300

    added ManyToOne and queries by nested properties example

---
+++ src/main/java/org/fasttrackit/onlineshopapi/repository/ProductRepository/ReviewRepository.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.onlineshopapi.repository.ProductRepository;
+
+
+import org.fasttrackit.onlineshopapi.domain.Product.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    //queries by nested properties
+    Page<Review> findbyProductId(Long productId, Pageable pageable);
+}
